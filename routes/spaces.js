a4065d273b722d941f7e9017d421dfe815ca9365
NKPToan27 <nguyenkhacphongt@gmail.com>
2024-09-18 00:19:07 +0700

    fix space BE

--- routes/spaces.js
+++ routes/spaces.js
@@ -1,4 +1,5 @@
 import { spaceController } from "../controllers/index.js";
+import express from "express";
 
 const spaceRouter = express.Router();
 spaceRouter.get("/", spaceController.getAllSpaces);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 import express, { json } from "express";
 
 import connectDB from "./database.js";
-import { userRouter } from "./routes/index.js";
+import { userRouter,spaceRouter } from "./routes/index.js";
 
 dotenv.config();
 //Tạo 1 constant 'app'
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcom to</h1>");
 });
 app.use("/users", userRouter);
+app.use("/spaces", spaceRouter);
 
 
 app.use(function (req, res, next) {
