061a623b9232643823ea46b7573318b6c9701214
kun_dev <cong.nguyen@vangxanh.net>
2024-09-18 23:25:34 +0700

    getListCategories

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import userRouter from "./users.js";
 import reviewRouter from "./reviews.js";
 import spaceRouter from "./spaces.js";
 import cartRouter from "./carts.js";
+import categoriesRouter from "./categories.js";
 
 export{
   userRouter,
   reviewRouter,
   spaceRouter,
-  cartRouter
+  cartRouter,
+  categoriesRouter
 }
