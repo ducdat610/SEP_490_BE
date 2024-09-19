80cb06cc1189c2157f624dfb61d33d3b774bf798
ToanNVF8 <ToanNVHE163111@gmail.com>
2024-09-19 19:32:57 +0700

    update

--- routes/categories.js
+++ routes/categories.js
@@ -17,4 +17,22 @@ categoriesRouter.get("/", async (req, res, next) => {
     res.status(200).json(categories);
   } catch (error) {}
 });
+categoriesRouter.post("/", async (req, res, next) => {
+  const { name, description } = req.body;
+  try {
+    const nameExit = await Categories.findOne({ name: name }).exec();
+    if (nameExit) {
+      return res
+        .status(400)
+        .json({ message: "Categories name already exists" });
+    }
+
+    const newCate = new Categories({ name, description });
+    await newCate.save();
+    res.status(200).json(newCate);
+  } catch (error) {
+    next(error);
+  }
+});
+categoriesRouter.put("/:id", async (req, res, next) => {});
 export default categoriesRouter;
