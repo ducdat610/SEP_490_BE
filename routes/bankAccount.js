42291737c4bac24cb7b50e10dfec77d463d5ca4c
ToanNVF8 <ToanNVHE163111@gmail.com>
2024-09-19 23:47:02 +0700

    up bank

--- routes/bankAccount.js
+++ routes/bankAccount.js
@@ -9,26 +9,29 @@ import {
 } from "../helpers/jwt_helper.js";
 import Users from "../models/users.js";
 const bankAccountRouter = express.Router();
-bankAccountRouter.post("/add-bank-account", async (req, res) => {
-  const { userId, bankName, accountNumber } = req.body;
+bankAccountRouter.post("/", async (req, res) => {
+  const { userId, bankId, accountNumber } = req.body;
 
   try {
-    // Tạo tài khoản ngân hàng mới
+    if (!userId || !bankId || !accountNumber) {
+      return res
+        .status(400)
+        .json({ message: "Thiếu trường bắt buộc trong yêu cầu" });
+    }
+
     const newBankAccount = new BankAccount({
-      bankName,
+      bankId,
+      userId,
       accountNumber,
-      user: userId, // Liên kết tài khoản ngân hàng với người dùng
     });
 
     await newBankAccount.save();
 
-    // Cập nhật tài liệu người dùng để thêm tài khoản ngân hàng mới
     const user = await Users.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "Người dùng không tìm thấy" });
     }
 
-    // Thêm ID của tài khoản ngân hàng mới vào mảng bankAccounts
     user.bankAccounts.push(newBankAccount._id);
     await user.save();
 
