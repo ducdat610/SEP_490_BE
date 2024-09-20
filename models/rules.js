2df8f59619d3c2fb59132b360b9d4e0ff595e3e5
kun_dev <cong.nguyen@vangxanh.net>
2024-09-20 22:30:15 +0700

    update model rules

--- models/rules.js
+++ models/rules.js
@@ -1,15 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
-const rulesSchema = new Schema({
-  text: {
-    type: String,
-    required: true,
+const rulesSchema = new Schema(
+  {
+    text: {
+      type: [String],
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Rules = mongoose.model("rules", rulesSchema);
 export default Rules;
