4d9e4c989ffc198f2e02210cfa43aec7b48b6e9d
Datnd <nguyenducdat0610@gmail.com>
2024-09-25 21:19:10 +0700

    update3

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/message", messageRouter);
 
 
 
-app.use(function (req, res, next) { // AAAA
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
